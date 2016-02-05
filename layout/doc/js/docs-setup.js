38494e36ea9cdeeb1067e051fee305703698f71d
Gilberto Leão <gilberto@jmjsistemas.com.br>
2016-02-05 17:29:12 -0300

    first commit

---
+++ layout/doc/js/docs-setup.js
@@ -0,0 +1,232 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "elHome",
+      "shortName": "elHome",
+      "type": "overview",
+      "moduleName": "elHome",
+      "shortDescription": "Módulo principal do componente Home que é responsável por exibir e gerenciar a tela inicial da aplicação.",
+      "keywords": "api aplica br charles codate componente da direitos elhome exibir gerenciar inicial os overview por principal reservados respons tela todos viegas"
+    },
+    {
+      "section": "api",
+      "id": "elHome",
+      "shortName": "elHome",
+      "type": "overview",
+      "moduleName": "elHome",
+      "shortDescription": "Módulo principal do componente Home que é responsável por exibir e gerenciar a tela inicial da aplicação.",
+      "keywords": "api aplica br charles codate componente da direitos elhome exibir gerenciar inicial os overview por principal reservados respons tela todos viegas"
+    },
+    {
+      "section": "api",
+      "id": "elHome",
+      "shortName": "elHome",
+      "type": "overview",
+      "moduleName": "elHome",
+      "shortDescription": "Módulo principal do componente Home que é responsável por exibir e gerenciar a tela inicial da aplicação.",
+      "keywords": "api aplica br charles codate componente da direitos elhome exibir gerenciar inicial os overview por principal reservados respons tela todos viegas"
+    },
+    {
+      "section": "api",
+      "id": "elHome",
+      "shortName": "elHome",
+      "type": "overview",
+      "moduleName": "elHome",
+      "shortDescription": "Módulo principal do componente Home que é responsável por exibir e gerenciar a tela inicial da aplicação.",
+      "keywords": "api aplica codate componente cunha da direitos elhome enieber exibir gerenciar inicial os overview por principal reservados respons tela todos"
+    },
+    {
+      "section": "api",
+      "id": "elHome",
+      "shortName": "elHome",
+      "type": "overview",
+      "moduleName": "elHome",
+      "shortDescription": "Módulo principal do componente Home que é responsável por exibir e gerenciar a tela inicial da aplicação.",
+      "keywords": "api aplica br charles codate componente da direitos elhome exibir gerenciar inicial os overview por principal reservados respons tela todos viegas"
+    },
+    {
+      "section": "api",
+      "id": "elHome",
+      "shortName": "elHome",
+      "type": "overview",
+      "moduleName": "elHome",
+      "shortDescription": "Módulo principal do componente Home que é responsável por exibir e gerenciar a tela inicial da aplicação.",
+      "keywords": "api aplica br charles codate componente da direitos elhome exibir gerenciar inicial os overview por principal reservados respons tela todos viegas"
+    },
+    {
+      "section": "api",
+      "id": "elParametro",
+      "shortName": "elParametro",
+      "type": "overview",
+      "moduleName": "elParametro",
+      "shortDescription": "Módulo principal do componente Home que é responsável",
+      "keywords": "angular api aplica br charles codate componente configura da de direitos elparametro exibir function gerenciar inicializa initialize modulos objeto os overview par por principal registro reservados respons retornando servi tela todos um viegas"
+    },
+    {
+      "section": "api",
+      "id": "elParametro.elParametroController",
+      "shortName": "elParametro.elParametroController",
+      "type": "object",
+      "moduleName": "elParametro",
+      "shortDescription": "Controlador principal do componente do parâmetros.",
+      "keywords": "$scope ap api aplica atualmente br charles codate componente controlador da dados de direitos elparametro elparametrocontroller elparametroevent elparametroservice function inicializa initialize lista logo mantidos necess object os par parametroatual parametros pelo principal property recuperado recuperando reposit reservados selecionado todos viegas"
+    },
+    {
+      "section": "api",
+      "id": "elParametro.elParametroService",
+      "shortName": "elParametro.elParametroService",
+      "type": "service",
+      "moduleName": "elParametro",
+      "shortDescription": "Serviço responsável pela recuperação e manutenção",
+      "keywords": "$http altera alterados alterar api aplica br buscar charles cliente codate contendo crit da dados de direitos dos elparametro elparametroservice entre filtrados filtrar filtro filtros identificador informado insere inserir lista manuten method novo objeto os par parametro pela pelo pesquisa promessa quando recupera registrados reposit requisi reservados resolvida respons retorna ser serem servi service servidor todos um uma vai viegas"
+    },
+    {
+      "section": "api",
+      "id": "jmjAutocomplete",
+      "shortName": "jmjAutocomplete",
+      "type": "overview",
+      "moduleName": "jmjAutocomplete",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um input na tela recebe os parametros:",
+      "keywords": "$parent api aria-describedby componente criar direitos diretiva elemento exibe gilbertole html input jmj jmjautocomplete label maxlength minlength model ng-model onde os overview parametros passa passado pelo placeholder por radio recebe reservados resonsav selecionado ser tela text todos type um valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjConsulta",
+      "shortName": "jmjConsulta",
+      "type": "overview",
+      "moduleName": "jmjConsulta",
+      "shortDescription": "Módulo principal do componente Home que é responsável por exibir e gerenciar a tela inicial da aplicação.",
+      "keywords": "api aplica componente da direitos exibir gerenciar gilbertole inicial jmj jmjconsulta os overview por principal reservados respons tela todos"
+    },
+    {
+      "section": "api",
+      "id": "jmjConsulta.jmjConsultaController",
+      "shortName": "jmjConsulta.jmjConsultaController",
+      "type": "object",
+      "moduleName": "jmjConsulta",
+      "shortDescription": "Controlador principal da consulta.",
+      "keywords": "$scope api consulta controlador da direitos gilbertole jmj jmjconsulta jmjconsultacontroller object os principal reservados todos"
+    },
+    {
+      "section": "api",
+      "id": "jmjConsulta.jmjConsultaController",
+      "shortName": "jmjConsulta.jmjConsultaController",
+      "type": "object",
+      "moduleName": "jmjConsulta",
+      "shortDescription": "Controlador principal da consulta.",
+      "keywords": "$scope api consulta controlador da direitos gilbertole jmj jmjconsulta jmjconsultacontroller object os principal reservados todos"
+    },
+    {
+      "section": "api",
+      "id": "jmjEnumLabel",
+      "shortName": "jmjEnumLabel",
+      "type": "overview",
+      "moduleName": "jmjEnumLabel",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um radio na tela recebe os parametros:",
+      "keywords": "$parent ao api aria-describedby atribuido campo checkbox classe como componente criar cunha define direitos diretiva elemento enieber entre enum espacamento exibe exibida exibidas iniciado jmj jmjenumlabel label model ng-model objeto onde op os overview padrao parametros passa passado pelo por propriedade radio radiobuttom recebe reservados resonsav selecionado ser sera tela todos type um usada valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjInputLabalHorizontal",
+      "shortName": "jmjInputLabalHorizontal",
+      "type": "overview",
+      "moduleName": "jmjInputLabalHorizontal",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um input na tela Horizontalmente recebe os parametros:",
+      "keywords": "$parent api aria-describedby checkbox componente criar direitos diretiva elemento enieber enum exibe horizontalmente html input jmj jmjinputlabalhorizontal label maxlength minlength model ng-model onde os overview parametros passa passado pelo placeholder por radio recebe reservados resonsav selecionado ser tela text todos type um valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjInputLabel",
+      "shortName": "jmjInputLabel",
+      "type": "overview",
+      "moduleName": "jmjInputLabel",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um input na tela recebe os parametros:",
+      "keywords": "$parent api aria-describedby checkbox componente criar direitos diretiva elemento enum exibe gilbertole html input jmj jmjinputlabel label maxlength minlength model ng-model onde os overview parametros passa passado pelo placeholder por radio recebe reservados resonsav selecionado ser tela text todos type um valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjPanel",
+      "shortName": "jmjPanel",
+      "type": "overview",
+      "moduleName": "jmjPanel",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um panel na tela recebe os parametros:",
+      "keywords": "api class componente criar direitos diretiva elemento exibe gilbertoleao jmj jmjpanel label ng-transclude os overview panel panel-body panel-default parametros passado pelo por recebe reservados resonsav tela todos um valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjPanel",
+      "shortName": "jmjPanel",
+      "type": "overview",
+      "moduleName": "jmjPanel",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um panel na tela recebe os parametros:",
+      "keywords": "api class componente criar direitos diretiva elemento exibe gilbertoleao jmj jmjpanel label ng-transclude os overview panel panel-body panel-default parametros passado pelo por recebe reservados resonsav tela todos um valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjPanel",
+      "shortName": "jmjPanel",
+      "type": "overview",
+      "moduleName": "jmjPanel",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um panel na tela recebe os parametros:",
+      "keywords": "api class componente criar direitos diretiva elemento exibe form-control gilbertoleao jmj jmjpanel label model ng-model os overview panel parametros passado pelo por recebe reservados resonsav selecione tela todos um valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjSelectLabel",
+      "shortName": "jmjSelectLabel",
+      "type": "overview",
+      "moduleName": "jmjSelectLabel",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um select na tela recebe os parametros:",
+      "keywords": "api class componente criar direitos diretiva elemento enieber exibe exibidas form-control jmj jmjselectlabel label model ng-model onde op os overview parametros passa passado pelo por recebe reservados resonsav selecionado selecione select ser tela todos um valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjSelectLabel",
+      "shortName": "jmjSelectLabel",
+      "type": "overview",
+      "moduleName": "jmjSelectLabel",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um select na tela recebe os parametros:",
+      "keywords": "api class componente criar direitos diretiva elemento enieber exibe exibidas form-control jmj jmjselectlabel label model ng-model onde op os overview parametros passa passado pelo por recebe reservados resonsav selecionado selecione select ser tela todos um valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjSPSADTService",
+      "shortName": "jmjSPSADTService",
+      "type": "service",
+      "moduleName": "jmjSPSADTService",
+      "shortDescription": "Serviço responsável pela recuperação e manutenção",
+      "keywords": "$http api autoriza consultas da de direitos enieber execu jmj jmjspsadtservice manuten os pela recupera reservados respons servi service todos"
+    },
+    {
+      "section": "api",
+      "id": "jmjTextLabel",
+      "shortName": "jmjTextLabel",
+      "type": "overview",
+      "moduleName": "jmjTextLabel",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um input na tela recebe os parametros:",
+      "keywords": "$parent api aria-describedby checkbox componente criar direitos diretiva elemento enum exibe gilbertole html input jmj jmjtextlabel label maxlength minlength model ng-model onde os overview parametros passa passado pelo placeholder por radio recebe reservados resonsav selecionado ser tela text todos type um valor"
+    },
+    {
+      "section": "api",
+      "id": "jmjValidate",
+      "shortName": "jmjValidate",
+      "type": "overview",
+      "moduleName": "jmjValidate",
+      "shortDescription": "Diretiva resonsavél por criar um elemento label e um input na tela recebe os parametros:",
+      "keywords": "$parent api aria-describedby componente criar direitos diretiva elemento exibe gilbertole html input jmj jmjvalidate label maxlength minlength model ng-model onde os overview parametros passa passado pelo placeholder por radio recebe reservados resonsav selecionado ser tela text todos type um valor"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
